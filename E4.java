18b829323f9bf85405c25347305039c2137b2722
Sankalp Srivastava <32612871+Goku1999@users.noreply.github.com>
2020-06-02 14:47:37 +0530

    Create E4.java

---
+++ E4.java
@@ -0,0 +1,57 @@
+/*
+Find the largest palindrome made from the product of two 3-digit numbers which is less than N.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler004/problem
+*/
+
+
+import java.util.Scanner;
+class E4
+{
+    public static void main(String ags[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        boolean ar[]=palins();
+        for(int x=0;x<t;x++)
+        {
+            int n=sc.nextInt();
+            for(int y=n-1;y>=0;y--)
+            {
+                if(ar[y])
+                {
+                    System.out.println(y);
+                    break;
+                }
+            }
+        }
+    }
+    public static boolean[] palins()
+    {
+        boolean ar[]=new boolean[10000001];//10^6+1 because aray starts with index 0
+        for(int x=100;x<=999;x++)
+        {
+            for(int y=100000/x;y<=999;y++)
+            {
+                int t=x*y;
+                if(isPalin(t))
+                {
+                    ar[t]=true;
+                }
+            }
+        }
+        return ar;
+    }
+    public static boolean isPalin(int n)
+    {
+        int t=n;
+        int r=0;
+        while(t!=0)
+        {
+            r*=10;
+            r+=t%10;
+            
+            t/=10;
+        }
+        return n==r;
+    }
+}
